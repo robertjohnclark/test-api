91b664e43daca63886f8e590cc7d25497ce33837
robertjohnclark <82611133+robertjohnclark@users.noreply.github.com>
2025-05-07 15:14:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
   return {"message": "Hello, Azure Web App!"}
+
+@app.get("/square/{number}")
+def get_square(number: int):
+    return {"number": number, "square": number ** 2}
   
 
 if __name__ == "__main__":
